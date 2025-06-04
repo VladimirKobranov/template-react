e78dbb6295deab6e4bc4b7e077c0641bb5eb404d
VladimirKobranov <josephclturok@gmail.com>
2025-06-04 19:05:58 +0900

    port env usage in vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const env = loadEnv(process.cwd(), "")
+const PORT = env.VITE_PORT;
+console.log('port', PORT)
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
+  server: {
+    port: PORT,
+  }
 
 })
